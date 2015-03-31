beda153823e0133b06d09f9c6e42d5120c9d60aa
Bofeng Li <bofeng.li@outlook.com>
2015-03-31 15:36:27 +0800

    Simplify Air Boot.

--- examples/air-boot/src/main/java/cn/libofeng/air/examples/boot/web/controller/SampleController.java
+++ examples/air-boot/src/main/java/cn/libofeng/air/examples/boot/web/controller/SampleController.java
@@ -1,12 +1,10 @@
 package cn.libofeng.air.examples.boot.web.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
 public class SampleController {
 
     @RequestMapping("/")
